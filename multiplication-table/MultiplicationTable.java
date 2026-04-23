75d8ac688804e8f1eea5ef7d522961eae1aeb0ab
DuDu <2849198417@qq.com>
2026-04-23 22:30:50 +0800

    Add 99 multiplication table

---
+++ multiplication-table/MultiplicationTable.java
@@ -0,0 +1,11 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        // 99 Multiplication Table
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.printf("%d*%d=%-3d", i, j, i * j);
+            }
+            System.out.println();
+        }
+    }
+}
